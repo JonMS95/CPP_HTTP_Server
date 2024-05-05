4a567176c8d5364104a7c99b7270b229432ccdb2
JonMS95 <jon_portugalete@hotmail.com>
2024-05-05 16:44:59 +0200

    As -n/--NonBlocking function is boolean, it's better to leave as false by default, rendering it useless if not mentioned in the executable call.

--- Tests/Source_files/main.cpp
+++ Tests/Source_files/main.cpp
@@ -44,7 +44,7 @@
 #define NON_BLOCKING_CHAR                   'n'
 #define NON_BLOCKING_LONG                   "NonBlocking"
 #define NON_BLOCKING_DETAIL                 "Non blocking socket."
-#define NON_BLOCKING_DEFAULT_VALUE          true
+#define NON_BLOCKING_DEFAULT_VALUE          false
 
 /********* Secure connection *********/
 
@@ -161,7 +161,7 @@ int main(int argc, char** argv)
 
     HttpSetPathToResources(path_resources);
 
-    ServerSocketRun(server_port, max_clients_num, concurrency_enabled, false, secure_connection, path_cert, path_pkey, HttpServerRun);
+    ServerSocketRun(server_port, max_clients_num, concurrency_enabled, non_blocking, secure_connection, path_cert, path_pkey, HttpServerInteractFn);
 
     return 0;
 }
