ecd82f5d1bf8e45244b17e632945fc40551026e4
JonMS95 <jon_portugalete@hotmail.com>
2024-05-31 16:48:41 +0200

    Removed every class method definition that do not belong to the current file.

--- Source_files/HttpServer.cpp
+++ Source_files/HttpServer.cpp
@@ -5,7 +5,7 @@
 #include <netinet/in.h>     // INET_ADDRSTRLEN.
 #include <arpa/inet.h>      // sockaddr_in, inet_addr
 #include <unistd.h>
-#include "HttpServer_api.hpp"
+#include "HttpServer.hpp"
 #include "SeverityLog_api.h"
 #include "ServerSocket_api.h"
 
@@ -410,103 +410,6 @@ int HttpServer::WriteToClient(int& client_socket, const std::string& httpRespons
     }
 
     return end_connection;
-}
-
-std::string HttpInteract::path_to_resources = "";
-
-int HttpInteract::HttpServerFSM(HttpServer& http_server, int client_socket)
-{
-    bool keep_interacting       = true              ;
-    HTTP_RUN_FSM http_run_fsm   = HTTP_RUN_FSM_READ ;
-    std::string read_from_client                    ;
-    std::string httpResponse                        ;
-
-    std::map<const std::string, std::string> request_fields =
-    {
-        {"Method"                       , ""},
-        {"Requested resource"           , ""},
-        {"Protocol"                     , ""},
-        {"Host"                         , ""},
-        {"Connection"                   , ""},
-        {"Cache-Control"                , ""},
-        {"Upgrade-Insecure-Requests"    , ""},
-        {"User-Agent"                   , ""},
-        {"Accept"                       , ""},
-        {"Referer"                      , ""},
-        {"Accept-Encoding"              , ""},
-        {"Accept-Language"              , ""},
-    };
-
-    while(keep_interacting)
-    {
-        switch(http_run_fsm)
-        {
-            // First, try to read something from client.
-            // If client got disconnected while trying to read or any error happened, end_connection > 0.
-            // In that case, exit and wait for an incoming connection to happen again.
-            case HTTP_RUN_FSM_READ:
-            {
-                int end_connection = http_server.ReadFromClient(client_socket, read_from_client);
-                
-                if(end_connection < 0)
-                    http_run_fsm = HTTP_RUN_FSM_END_CONNECTION;
-                else
-                    http_run_fsm = HTTP_RUN_FSM_PROCESS_REQUEST;
-            }
-            break;
-
-            // Once something has been read, process the request. Erase the current request string afterwards,
-            // leaving enough space for potential incoming requests.
-            case HTTP_RUN_FSM_PROCESS_REQUEST:
-            {
-                int process_request = http_server.ProcessRequest(read_from_client, request_fields);
-                
-                // If request could not be processed properly, then stop interacting with the client.
-                if(process_request < 0)
-                    http_run_fsm = HTTP_RUN_FSM_END_CONNECTION;
-                else
-                    http_run_fsm = HTTP_RUN_FSM_GENERATE_RESPONSE;
-            }
-            break;
-
-            // After processing the request, generate a proper response.
-            case HTTP_RUN_FSM_GENERATE_RESPONSE:
-            {
-                unsigned long int response_size = http_server.GenerateResponse(request_fields.at("Requested resource"), httpResponse);
-
-                // If response could not be generated, exit and wait for an incoming connection to happen again.
-                if(response_size < 0)
-                    http_run_fsm = HTTP_RUN_FSM_END_CONNECTION;
-                else
-                    http_run_fsm = HTTP_RUN_FSM_WRITE;
-            }
-            break;
-
-            // Finally, send the generated response back to the client.
-            // If client got disconnected or any other kind of error happened while trying to wtite, then exit the process.
-            case HTTP_RUN_FSM_WRITE:
-            {
-                int write_to_client = http_server.WriteToClient(client_socket, httpResponse);
-
-                if(write_to_client < 0)
-                    http_run_fsm = HTTP_RUN_FSM_END_CONNECTION;
-                else
-                    http_run_fsm = HTTP_RUN_FSM_READ;
-            }
-            break;
-
-            case HTTP_RUN_FSM_END_CONNECTION:
-            {
-                keep_interacting = false;
-            }
-            break;
-
-            default:
-            break;
-        }
-    }
-
-    return 0;
 }
 
 /******************************************/
