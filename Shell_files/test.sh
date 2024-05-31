2198f40318bd40d235308e7b84aab2cc3fcad33a
JonMS95 <jon_portugalete@hotmail.com>
2024-05-31 16:46:25 +0200

    Using HTTP (not HTTPS) test by now.

--- Shell_files/test.sh
+++ Shell_files/test.sh
@@ -16,5 +16,7 @@ echo
 echo "*******************************"
 echo "Testing 'main' executable file."
 echo "*******************************"
-./Tests/Executable_files/main -r ${DEFAULT_USER_PORT} -t 1800 -p -m 10 -s -c ~/Desktop/scripts/certificate_test/certificate.crt -k ~/Desktop/scripts/certificate_test/private.key
-# ./Tests/Executable_files/main -r ${DEFAULT_USER_PORT} -t 100000 -s -c ~/Desktop/scripts/certificate_test/certificate.crt -k ~/Desktop/scripts/certificate_test/private.key
+# # HTTP
+./Tests/Executable_files/main -r ${DEFAULT_USER_PORT} -t 1800 -p -m 10
+# # HTTPS
+# ./Tests/Executable_files/main -r ${DEFAULT_USER_PORT} -t 1800 -p -m 10 -s -c ~/Desktop/scripts/certificate_test/certificate.crt -k ~/Desktop/scripts/certificate_test/private.key
