921a390aa8ec850f5929a9ed43a966081a68bba1
JonMS95 <jon_portugalete@hotmail.com>
2024-05-31 18:30:57 +0200

    Removed input parameters in many class methods. Added class variables in exchange.

--- Source_files/HttpServer.hpp
+++ Source_files/HttpServer.hpp
@@ -78,103 +78,28 @@ typedef enum
 /********** Class definition *********/
 /*************************************/
 
-const std::map<const std::string, const std::string> extension_to_content_type =
-{
-    {"aac"      ,	"audio/aac"                                                                 },
-    {"abw"      ,	"application/x-abiword"                                                     },
-    {"apng"     ,   "image/apng"                                                                },
-    {"arc"      ,	"application/x-freearc"                                                     },
-    {"avif"     ,	"image/avif"                                                                },
-    {"avi"      ,	"video/x-msvideo"                                                           },
-    {"azw"      ,	"application/vnd.amazon.ebook"                                              },
-    {"bin"      ,	"application/octet-stream"                                                  },
-    {"bmp"      ,	"image/bmp"                                                                 },
-    {"bz"       ,	"application/x-bzip"                                                        },
-    {"bz2"      ,	"application/x-bzip2"                                                       },
-    {"cda"      ,	"application/x-cdf"                                                         },
-    {"csh"      ,	"application/x-csh"                                                         },
-    {"css"      ,	"text/css"                                                                  },
-    {"csv"      ,	"text/csv"                                                                  },
-    {"doc"      ,	"application/msword"                                                        },
-    {"docx"     ,	"application/vnd.openxmlformats-officedocument.wordprocessingml.document"   },
-    {"eot"      ,	"application/vnd.ms-fontobject"                                             },
-    {"epub"     ,	"application/epub+zip"                                                      },
-    {"gz"       ,	"application/gzip"                                                          },
-    {"gif"      ,	"image/gif"                                                                 },
-    {"htm"      ,	"text/html"                                                                 },
-    {"html"     ,	"text/html"                                                                 },
-    {"ico"      ,	"image/vnd.microsoft.icon"                                                  },
-    {"ics"      ,	"text/calendar"                                                             },
-    {"jar"      ,	"application/java-archive"                                                  },
-    {"jpg"      ,	"image/jpeg"                                                                },
-    {"jpeg"     ,	"image/jpeg"                                                                },
-    {"js"       ,	"javascript"                                                                },
-    {"json"     ,	"application/json"                                                          },
-    {"jsonld"   ,	"application/ld+json"                                                       },
-    {"mid"      ,	"audio/x-midi"                                                              },
-    {"midi"     ,	"audio/x-midi"                                                              },
-    {"mjs"      ,	"text/javascript"                                                           },
-    {"mp3"      ,	"audio/mpeg"                                                                },
-    {"mp4"      ,	"video/mp4"                                                                 },
-    {"mpeg"     ,	"video/mpeg"                                                                },
-    {"mpkg"     ,	"application/vnd.apple.installer+xml"                                       },
-    {"odp"      ,	"application/vnd.oasis.opendocument.presentation"                           },
-    {"ods"      ,	"application/vnd.oasis.opendocument.spreadsheet"                            },
-    {"odt"      ,	"application/vnd.oasis.opendocument.text"                                   },
-    {"oga"      ,	"audio/ogg"                                                                 },
-    {"ogv"      ,	"video/ogg"                                                                 },
-    {"ogx"      ,	"application/ogg"                                                           },
-    {"opus"     ,	"audio/opus"                                                                },
-    {"otf"      ,	"font/otf"                                                                  },
-    {"png"      ,	"image/png"                                                                 },
-    {"pdf"      ,	"application/pdf"                                                           },
-    {"php"      ,	"application/x-httpd-php"                                                   },
-    {"ppt"      ,	"application/vnd.ms-powerpoint"                                             },
-    {"pptx"     ,	"application/vnd.openxmlformats-officedocument.presentationml.presentation" },
-    {"rar"      ,	"application/vnd.rar"                                                       },
-    {"rtf"      ,	"application/rtf"                                                           },
-    {"sh"       ,	"application/x-sh"                                                          },
-    {"svg"      ,	"image/svg+xml"                                                             },
-    {"tar"      ,	"application/x-tar"                                                         },
-    {"tif"      ,	"image/tiff"                                                                },
-    {"tiff"     ,	"image/tiff"                                                                },
-    {"ts"       ,	"video/mp2t"                                                                },
-    {"ttf"      ,	"font/ttf"                                                                  },
-    {"txt"      ,	"text/plain"                                                                },
-    {"vsd"      ,	"application/vnd.visio"                                                     },
-    {"wav"      ,	"audio/wav"                                                                 },
-    {"weba"     ,	"audio/webm"                                                                },
-    {"webm"     ,	"video/webm"                                                                },
-    {"webp"     ,	"image/webp"                                                                },
-    {"woff"     ,	"font/woff"                                                                 },
-    {"woff2"    ,	"font/woff2"                                                                },
-    {"xhtml"    ,	"application/xhtml+xml"                                                     },
-    {"xls"      ,	"application/vnd.ms-excel"                                                  },
-    {"xlsx"     ,	"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"         },
-    {"xml"      ,	"application/xml"                                                           },
-    {"xul"      ,	"application/vnd.mozilla.xul+xml"                                           },
-    {"zip"      ,	"application/zip"                                                           },
-    {"3gp"      ,	"video/3gpp"                                                                },
-    {"3g2"      ,	"video/3gpp2"                                                               },
-    {"7z"       ,	"application/x-7z-compressed"                                               },
-};
-
 class HttpServer
 {
 private:
     const std::string path_to_resources;
 
     using ext_to_type_table = const std::map<const std::string, const std::string>;
-    std::shared_ptr<ext_to_type_table> ptr_extension_to_content = std::make_shared<ext_to_type_table>(extension_to_content_type);
+    const std::shared_ptr<ext_to_type_table> ptr_extension_to_content;
+
+    using request_fields_table = std::map<const std::string, std::string>;
+    const std::shared_ptr<request_fields_table> ptr_request_fields;
+
+    std::string read_from_client;
+    std::string httpResponse    ;
 
     // Used by ReadFromClient
-    bool CheckRequestEnd(std::string& request);
+    bool CheckRequestEnd(void);
     
     // Used by ProcessRequest
     std::vector<std::string> ExtractWordsFromReqLine(const std::string& input);
 
     // Used by GenerateResponse
-    const std::string   CheckResourceToSend(const std::string& requested_resource)                          ;
+    const std::string   CheckResourceToSend(void)                          ;
     const std::string   GetPathToResources(void)                                                            ;
     bool                FileExists(const std::string& filePath)                                             ;
     std::string         GetFileExtension(const std::string& text)                                           ;
@@ -190,16 +115,16 @@ public:
     HttpServer(const HttpServer& obj) = delete;
 
     // Read data from client
-    int ReadFromClient(int& client_socket, std::string& read_from_client);
+    int ReadFromClient(int& client_socket);
 
     // Process request
-    int ProcessRequest(const std::string&read_from_client, std::map<const std::string, std::string>& request_fields);
+    int ProcessRequest(void);
 
     // Generate response for client
-    unsigned long int GenerateResponse(const std::string& requested_resource, std::string& httpResponse);
+    unsigned long int GenerateResponse();
 
     // Write to client
-    int WriteToClient(int& client_socket, const std::string& httpResponse);
+    int WriteToClient(int& client_socket);
 };
 
 /************************************/
