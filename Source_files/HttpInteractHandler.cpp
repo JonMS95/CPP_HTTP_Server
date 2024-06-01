dc3dea3ce6151541a817cb032d58eb0e4b7ea767
JonMS95 <jon_portugalete@hotmail.com>
2024-06-01 16:08:51 +0200

    Http server FSM has now been moved to HttpServer class under "Run" method. This class does only need to instantiate an Http server and run it.

--- Source_files/HttpInteractHandler.cpp
+++ Source_files/HttpInteractHandler.cpp
@@ -20,85 +20,6 @@
 
 std::string HttpInteractHandler::path_to_resources = "";
 
-int HttpInteractHandler::HttpServerFSM(HttpServer& http_server, int client_socket)
-{
-    bool keep_interacting       = true              ;
-    HTTP_RUN_FSM http_run_fsm   = HTTP_RUN_FSM_READ ;
-    std::string read_from_client                    ;
-    std::string httpResponse                        ;
-
-    while(keep_interacting)
-    {
-        switch(http_run_fsm)
-        {
-            // First, try to read something from client.
-            // If client got disconnected while trying to read or any error happened, end_connection > 0.
-            // In that case, exit and wait for an incoming connection to happen again.
-            case HTTP_RUN_FSM_READ:
-            {
-                int end_connection = http_server.ReadFromClient(client_socket);
-                
-                if(end_connection < 0)
-                    http_run_fsm = HTTP_RUN_FSM_END_CONNECTION;
-                else
-                    http_run_fsm = HTTP_RUN_FSM_PROCESS_REQUEST;
-            }
-            break;
-
-            // Once something has been read, process the request. Erase the current request string afterwards,
-            // leaving enough space for potential incoming requests.
-            case HTTP_RUN_FSM_PROCESS_REQUEST:
-            {
-                int process_request = http_server.ProcessRequest();
-                
-                // If request could not be processed properly, then stop interacting with the client.
-                if(process_request < 0)
-                    http_run_fsm = HTTP_RUN_FSM_END_CONNECTION;
-                else
-                    http_run_fsm = HTTP_RUN_FSM_GENERATE_RESPONSE;
-            }
-            break;
-
-            // After processing the request, generate a proper response.
-            case HTTP_RUN_FSM_GENERATE_RESPONSE:
-            {
-                unsigned long int response_size = http_server.GenerateResponse();
-
-                // If response could not be generated, exit and wait for an incoming connection to happen again.
-                if(response_size < 0)
-                    http_run_fsm = HTTP_RUN_FSM_END_CONNECTION;
-                else
-                    http_run_fsm = HTTP_RUN_FSM_WRITE;
-            }
-            break;
-
-            // Finally, send the generated response back to the client.
-            // If client got disconnected or any other kind of error happened while trying to wtite, then exit the process.
-            case HTTP_RUN_FSM_WRITE:
-            {
-                int write_to_client = http_server.WriteToClient(client_socket);
-
-                if(write_to_client < 0)
-                    http_run_fsm = HTTP_RUN_FSM_END_CONNECTION;
-                else
-                    http_run_fsm = HTTP_RUN_FSM_READ;
-            }
-            break;
-
-            case HTTP_RUN_FSM_END_CONNECTION:
-            {
-                keep_interacting = false;
-            }
-            break;
-
-            default:
-            break;
-        }
-    }
-
-    return 0;
-}
-
 void HttpInteractHandler::SetPathToResources(const char* path_to_resources)
 {
     HttpInteractHandler::path_to_resources = path_to_resources;
@@ -106,9 +27,9 @@ void HttpInteractHandler::SetPathToResources(const char* path_to_resources)
 
 int HttpInteractHandler::InteractFn(int client_socket)
 {
-    HttpServer server(HttpInteractHandler::path_to_resources);
+    HttpServer http_server(HttpInteractHandler::path_to_resources);
 
-    return HttpInteractHandler::HttpServerFSM(server, client_socket);
+    return http_server.Run(client_socket);
 }
 
 /******************************************/
